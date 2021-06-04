bebb26c3dbe4c57e93e75be1cc758b3b7704a558
vivisuke <ntsuda@master.email.ne.jp>
2021-06-05 05:10:08 +0900

    0064 ◎ C 06/05 06/05 0.001│ToDo│1問用のパネルシーン追加 0065 ◎ C 06/05 06/05 0.001│ToDo│グローバル変数用シーン追加

---
+++ GDNonogram15/QuestPanel.gd
@@ -0,0 +1,23 @@
+extends ReferenceRect
+
+const RADIUS = 10
+const POSITION = Vector2(2, 2)
+const SIZE = Vector2(450-10, 90)
+const THUMBNAIL_WIDTH = 15*4
+const THUMBNAIL_POS = (90-THUMBNAIL_WIDTH)/2+2
+
+func _ready():
+	pass # Replace with function body.
+
+func _draw():
+	# 外枠
+	var style_box = StyleBoxFlat.new()
+	style_box.set_corner_radius_all(RADIUS)
+	style_box.border_color = Color.green
+	style_box.set_border_width_all(2)
+	style_box.shadow_offset = Vector2(4, 4)
+	style_box.shadow_size = 8
+	draw_style_box(style_box, Rect2(POSITION, SIZE))
+	# サムネイル
+	draw_rect(Rect2(THUMBNAIL_POS, THUMBNAIL_POS, THUMBNAIL_WIDTH, THUMBNAIL_WIDTH), Color.white)
+
